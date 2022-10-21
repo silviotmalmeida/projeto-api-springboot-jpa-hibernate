076210100da6b180d6373a9f3f8bea6f4e45ef1e
silviotmalmeida <silviotmalmeida@gmail.com>
2022-10-21 14:01:47 -0300

    Finalizando o projeto

--- app/src/main/java/com/silviotmalmeida/app/resources/exceptions/ResourceExceptionHandler.java
+++ app/src/main/java/com/silviotmalmeida/app/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.silviotmalmeida.app.services.exceptions.DatabaseException;
 import com.silviotmalmeida.app.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -26,6 +27,25 @@ public class ResourceExceptionHandler {
 
         // definindo o código do erro
         HttpStatus status = HttpStatus.NOT_FOUND;
+
+        // criando o erro customizado
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+                request.getRequestURI());
+
+        // retornando o erro customizado
+        return ResponseEntity.status(status).body(err);
+    }
+
+    // método que vai interceptar e tratar todas as exceções do tipo
+    // DatabaseException
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+
+        // definindo o erro
+        String error = "Database error";
+
+        // definindo o código do erro
+        HttpStatus status = HttpStatus.BAD_REQUEST;
 
         // criando o erro customizado
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
--- app/src/main/java/com/silviotmalmeida/app/services/UserService.java
+++ app/src/main/java/com/silviotmalmeida/app/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.silviotmalmeida.app.entities.User;
 import com.silviotmalmeida.app.repositories.UserRepository;
+import com.silviotmalmeida.app.services.exceptions.DatabaseException;
 import com.silviotmalmeida.app.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 // classe de serviço que realiza a comunicação entre o UserResource e o UserRepository
 // registrando a classe como service
@@ -30,7 +35,7 @@ public class UserService {
 		// obtendo o registro
 		Optional<User> obj = this.repository.findById(id);
 
-		// se existir, retorna o registro, senão lança uma exceção
+		// se existir, retorna o registro, senão lança uma exceção personalizada
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
@@ -44,22 +49,41 @@ public class UserService {
 	// método que remove um registro no BD
 	public void delete(Long id) {
 
-		// retorna o registro, após a inserção
-		this.repository.deleteById(id);
+		// tratando exceções
+		try {
+			// retorna o registro, após a remoção
+			this.repository.deleteById(id);
+		}
+		// em caso de id não encontrado, lança a exceção personalizada
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		// em caso de id com pedidos, lança a exceção personalizada
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	// método que edita um registro no BD
 	public User update(Long id, User obj) {
 
-		// preparando um objeto monitorado
-		// é mais performático do que o findById
-		User entity = this.repository.getReferenceById(id);
+		// tratando exceções
+		try {
 
-		// editando os atributos permitidos
-		updateData(entity, obj);
+			// preparando um objeto monitorado
+			// é mais performático do que o findById
+			User entity = this.repository.getReferenceById(id);
 
-		// retorna o registro, após a edição
-		return this.repository.save(entity);
+			// editando os atributos permitidos
+			updateData(entity, obj);
+
+			// retorna o registro, após a edição
+			return this.repository.save(entity);
+		}
+		// em caso de id não encontrado, lança a exceção personalizada
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	// método auxiliar para realizar a edição de atributos permitidos de um registro
